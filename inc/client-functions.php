9220e9ccfb5b49dbb8a8c492b93c7536be15e06d
COVERT NINE <tim@covertnine.com>
2020-03-28 03:04:13 -0500

    hiding unneccessary font setting

--- inc/client-functions.php
+++ inc/client-functions.php
@@ -55,6 +55,19 @@ if (!function_exists('c9_client_setup')) {
 }
 
 /****************************************************************************************/
+/* C9 Music Admin Settings */
+/****************************************************************************************/
+if ( ! function_exists( 'c9_hide_font_setting' ) ) {
+	/**
+	 * Hide admin setting for font cause we already set those in CSS
+	 */
+	function c9_hide_font_setting() {
+		wp_enqueue_style( 'c9-client-admin-styles', get_template_directory_uri() . '/client/client-assets/dist/client-admin.css' );
+	}
+	add_action( 'admin_print_styles', 'c9_hide_font_setting', 999);
+} //end if function exists
+
+/****************************************************************************************/
 /* WooCommerce */
 /****************************************************************************************/
 include("woocommerce-functions.php");
