1338bc44d95cb44f52a4a17761078e1fb7e592f2
COVERT NINE <tim@covertnine.com>
2020-03-30 22:32:57 -0500

    cleaning up comments

--- client-assets/custom-client.js
+++ client-assets/custom-client.js
@@ -1,45 +1,5 @@
 jQuery(document).ready(function() {
   (function($) {
-    // // add any css classes you don't want to be smoothState-d here
-    // var notSmooth =
-    //   '.nav-search, btn-nav-search, .search-close, .mega-menu-item-has-children .mega-menu-link, .wp-block-gallery a[href$=".jpg"], .wp-block-gallery a[href$=".jpeg"], .wp-block-gallery a[href$=".png"], .wp-block-gallery a[href$=".gif, "], .cortex-popup';
-    // var options = {
-    //     prefetch: true,
-    //     cacheLength: 2,
-    //     blacklist: notSmooth,
-    //     onStart: {
-    //       duration: 250, // Duration of our animation
-    //       render: function($container) {
-    //         // Add your CSS animation reversing class
-    //         $container.addClass("is-exiting");
-    //         // Restart your animation
-    //         smoothState.restartCSSAnimations();
-    //       }
-    //     },
-    //     onReady: {
-    //       duration: 0,
-    //       render: function($container, $newContent) {
-    //         // Remove your CSS animation reversing class
-    //         $container.removeClass("is-exiting");
-    //         // Inject the new content
-    //         $container.html($newContent);
-    //       }
-    //     }
-    //   },
-    //   smoothState = $("#page")
-    //     .smoothState(options)
-    //     .data("smoothState");
-    // document
-    //   .querySelectorAll(".navbar-nav > .nav-item > .nav-link")
-    //   .forEach(btn => {
-    //     btn.addEventListener("click", () => {
-    //       gsap.to(window, {
-    //         duration: 3,
-    //         scrollTo: { href, offsetY: 70 }
-    //       });
-    //     });
-    //   });
-
     $("body").on(
       "click",
       ".nav-link[href^='/#'], .scroll-me a[href^='/#'], .dropdown-item[href^='/#']",
