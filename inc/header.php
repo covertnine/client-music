bb0427144406b55cdd17f67e0d3a1ea97a7f43d3
tim <tim@covertnine.com>
2020-12-15 19:55:04 -0600

    adding cart icon to header

--- inc/header.php
+++ inc/header.php
@@ -20,7 +20,22 @@
 					</div>
 				<?php
 				}
-				if (has_nav_menu('primary')) { ?>
+				?>
+				<div class="nav-order d-inline-block d-lg-none">
+					<?php
+					if (defined('WC_VERSION')) { //show cart contents if woo is active
+						$count = WC()->cart->get_cart_contents_count();
+
+						//if there are items in the cart, put a number in front of the icon
+						if ($count != 0) {
+							echo '<div class="nav-woocommerce" itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement"><a href="' . esc_url(wc_get_cart_url()) . '" title="' . esc_attr__('Shopping Cart', 'c9-togo') . '" class="nav-link nav-shop-link"><span class="sr-only">' . esc_html__('View Cart', 'c9-togo') . '</span> <i class="fa fa-shopping-cart fa-md"></i><span class="count">' . $count . '</span></a></div>';
+						} else { //if not just put in an icon
+							echo '<div class="nav-woocommerce" itemscope="itemscope" itemtype="https://www.schema.org/SiteNavigationElement"><a href="' . esc_url(wc_get_cart_url()) . '" title="' . esc_attr__('Shopping Cart', 'c9-togo') . '" class="nav-link nav-shop-link"><i class="fa fa-shopping-cart fa-md"></i> <span class="sr-only">' . esc_html__('View Cart', 'c9-togo') . '</span></a></div>';
+						} //end count check
+					} //end if woocommerce is active
+					?>
+				</div>
+				<?php if (has_nav_menu('primary')) { ?>
 
 					<div class="nav-toggle">
 						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e('Toggle Navigation', 'c9-music'); ?>">
